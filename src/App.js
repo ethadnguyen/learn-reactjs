bc55894416c7b7d5d5488cf44647d88ce2cdf230
ethadnguyen <tringuyen1382003@gmail.com>
2022-11-18 12:51:01 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ethad nguyen
         </a>
       </header>
     </div>
